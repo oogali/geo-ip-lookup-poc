8315184ed771950c3cd5861b4791e8fa7cc81348
Omachonu Ogali <oogali@idlepattern.com>
2014-09-14 14:50:29 -0400

    Fix syntax error

--- poc.py
+++ poc.py
@@ -30,7 +30,7 @@ def main(argv=None):
         argv = sys.argv
 
     if len(argv) < 2:
-        print '{} <ip>'.formation(argv[0])
+        print '{} <ip>'.format(argv[0])
 
     databases = [
         'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz',
